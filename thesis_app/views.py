0fcfbc90b0002e07735711f14be7f5a38433b6eb
QueencessMay <munsayac.queencessmay@gmail.com>
2023-11-06 20:17:55 +0800

    added upload form in multiple sentiment page

--- thesis_app/views.py
+++ thesis_app/views.py
@@ -1,5 +1,6 @@
 # sentiment/views.py
 from django.shortcuts import render
+from .forms import CSVForm
 import tensorflow as tf
 from transformers import DistilBertTokenizerFast, TFDistilBertForSequenceClassification
 import random
@@ -11,7 +12,7 @@ import random
 def single(request):
   if request.method == "POST":
     text_input = request.POST.get('textarea_input', '')
-    text_output = perform_sentiment_analysis(text_input)
+    text_output = analyze_single_sentiment(text_input)
     context = {
       'result': text_output,
       'user_input': text_input,
@@ -21,11 +22,32 @@ def single(request):
     return render(request, 'thesis_app/single.html')
 
 def multi(request):
-  return render(request, "thesis_app/multi.html")
+  if request.method == 'POST':
+    form = CSVForm(request.POST, request.FILES)
+    if form.is_valid():
+      file_input = request.FILES['csv']
+      file_output = analyze_multiple_sentiment(file_input)
+      context = {
+        'num_positive': file_output['num_positive'],
+        'num_negative': file_output['num_negative'],
+      }
+      return render(request, "thesis_app/multi.html", context)
+  else:
+    form = CSVForm()
+  return render(request, "thesis_app/multi.html", { 'form': form })
 
 def home(request):
   return single(request)
 
-def perform_sentiment_analysis(sentence):
+# Baguhin tong mga functions
+def analyze_multiple_sentiment(file):
+  # Insert our amazing sentiment analyzer here + File Reader XD
+  results = {
+    'num_positive': 5,
+    'num_negative': 5,
+  } # Nagbabago or smth
+  return results 
+
+def analyze_single_sentiment(input):
   # Insert our amazing sentiment analyzer here
   return random.choice([-1, 1]) # Number na lang ireturn nyo ah: 1 Positive, -1 Negative, 0 Default
