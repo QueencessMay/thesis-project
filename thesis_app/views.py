86f4c095bb390d5e1a230b246ca85ab6ddcdeb4d
QueencessMay <munsayac.queencessmay@gmail.com>
2023-11-18 21:25:24 +0800

    finished multiple sentiment page

--- thesis_app/views.py
+++ thesis_app/views.py
@@ -1,17 +1,17 @@
 # sentiment/views.py
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.apps import apps
 from .forms import CSVForm
+import pandas as pd
 import tensorflow as tf
-from transformers import DistilBertTokenizerFast, TFDistilBertForSequenceClassification
 from . import Preprocessing
 
-# Analyze sentiment of one game review with or without emojis and emoticons
+
+# Views for one game review with or without emojis and emoticons
 
 def view_single_without(request):
   if request.method == "POST":
-    path = 'FiouReia/distilbert-uncased-without-emojis-emoticons'
-    model = TFDistilBertForSequenceClassification.from_pretrained(path)
-
+    model = apps.get_app_config('thesis_app').model_without
     text_input = request.POST.get('textarea_input', '')
     text_output = analyze_single(text_input, model)
 
@@ -25,9 +25,7 @@ def view_single_without(request):
 
 def view_single_with(request):
   if request.method == "POST":
-    path = 'FiouReia/distilbert-uncased-with-emojis-emoticons'
-    model = TFDistilBertForSequenceClassification.from_pretrained(path)
-
+    model = apps.get_app_config('thesis_app').model_with
     text_input = request.POST.get('textarea_input', '')
     text_input_preprocessed = Preprocessing.preprocess_text(text_input)
     text_output = analyze_single(text_input_preprocessed, model)
@@ -39,48 +37,63 @@ def view_single_with(request):
     return render(request, 'thesis_app/single-with.html', context)
   else:
     return render(request, 'thesis_app/single-with.html')
-  
+
 def analyze_single(input, model):
   if not input:
     return -1
-  tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+  tokenizer = apps.get_app_config('thesis_app').tokenizer
   inputs = tokenizer(input, return_tensors="tf")
   outputs = model(inputs)
   sentiment = tf.argmax(outputs.logits, axis=1).numpy()[0]
   return sentiment
 
-# Analyze sentiment of multiple game reviews with or without emojis and emoticons
 
-def multi(request):
+# Views for multiple game reviews with or without emojis and emoticons
+
+def view_multi(request):
   if request.method == 'POST':
     form = CSVForm(request.POST, request.FILES)
-
     if form.is_valid():
       file_input = request.FILES['csv']
-
-      # if file_input:
-      #   if file_input.name.endswith('.csv'):
-      #     try:
-      #       csv.reader(file_input)
-      #     except csv.Error:
-      #       return render(request, "thesis_app/multi.html", { 'isError': True })
-
-      file_output = analyze_multiple_sentiment(file_input)
-      context = {
-        'num_positive': file_output['num_positive'],
-        'num_negative': file_output['num_negative'],
-      }
-      return render(request, "thesis_app/multi.html", context)
+      file_output = analyze_file(file_input)
+      request.session['file_output'] = file_output
+      return redirect('multiple_result')
   else:
     form = CSVForm()
-  return render(request, "thesis_app/multi.html", { 'form': form })
+  return render(request, "thesis_app/multi.html", { 'form': form }) 
 
-def analyze_multiple_sentiment(file):
-  results = {
-    'num_positive': 5,
-    'num_negative': 5,
-  } 
-  return results 
+def view_multi_result(request):
+  file_result = request.session.get('file_output', [])
+  return render(request, "thesis_app/multi-result.html", { 'result': file_result })
+
+
+# Helper functions
+
+def analyze_file(file):
+  model = apps.get_app_config('thesis_app').model_with # Currently for with emoji and emoticons
+  results = []
+
+  for batch in pd.read_csv(file, header=None, names=['review_col'], chunksize=500):
+    reviews = batch['review_col'].tolist()
+    preprocessed_reviews = [Preprocessing.preprocess_text(text) for text in reviews]
+    sentiments = analyze_batch(preprocessed_reviews, model)
+    batch_results = [
+      {
+        'review': review,
+        'sentiment': "negative" if sentiment == 0 else "positive"
+      }
+      for review, sentiment in zip(reviews, sentiments)
+    ]
+    results.extend(batch_results)
+
+  return results
+
+def analyze_batch(reviews, model):
+  tokenizer = apps.get_app_config('thesis_app').tokenizer
+  inputs = tokenizer(reviews, return_tensors="tf", padding=True, truncation=True)
+  outputs = model(inputs)
+  sentiments = tf.argmax(outputs.logits, axis=1).numpy().tolist()
+  return sentiments
 
 def home(request):
   return view_single_without(request)
